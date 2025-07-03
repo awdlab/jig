d1f3fa8f0d9d570b251b41527bcf8e1bcd010980
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2025-07-03 18:00:41 +0200

    sticky scroller items

--- packages/controls/src/scroller/scroller.ts
+++ packages/controls/src/scroller/scroller.ts
@@ -25,25 +25,58 @@ import { fromEvent, map } from 'rxjs';
   },
 })
 export class Scroller<T> {
-  public readonly virtual = input<boolean | undefined>(false);
-  public readonly itemHeight = input<number>(0);
-  public readonly padding = input<number>(0);
-  private readonly _appliedPadding = computed(() => this.padding() + 2);
+  /**
+   * The items to be displayed in the scroller.
+   * This is a required input and should be an array of items of type T.
+   */
   public readonly items = input.required<readonly T[]>();
+  /**
+   * Whether the scroller should use virtual scrolling.
+   * When set to true, the scroller will only render the items that are currently visible
+   * on the screen, improving performance for large lists.
+   *
+   * When virtual scrolling is enabled, {@link itemHeight} must be set to a non-zero value.
+   * @defaultValue `false`
+   */
+  public readonly virtual = input<boolean | undefined>(false);
+  /**
+   * When {@link virtual} scrolling is enabled, this input is required to define the height of each item in pixels.
+   */
+  public readonly itemHeight = input<number>(0);
+  /**
+   * When {@link virtual} scrolling is enabled, this input defines the number of items to be rendered above and below the visible area.
+   * This can help to reduce flickering when scrolling.
+   * @defaultValue `2`
+   */
+  public readonly padding = input<number>(2);
+  /**
+   * Determines whether the scroller should have a tab index and be focusable.
+   * @defaultValue `false`
+   */
   public readonly focusable = input<boolean>(false);
+  /**
+   * Determines whether an item is sticky or not.
+   * If set, the scroller will stick the items with a truthy value for the specified field to the top of the scroller.
+   */
+  public readonly fieldSticky = input<keyof T | null>(null);
 
   private readonly _itemList = viewChild.required<ElementRef<HTMLElement>>('itemList');
+  /**
+   * The template to be used for rendering each item in the scroller.
+   * Can also be set using the `item` content child.
+   */
+  public readonly templateItem = input<TemplateRef<typeof this.templateTypes.item> | null>(null);
   private readonly _userItemTemplate =
     contentChild<TemplateRef<typeof this.templateTypes.item>>('item');
-  public readonly templateItem = input<TemplateRef<typeof this.templateTypes.item> | null>(null);
   protected readonly itemTemplate = computed(() => this._userItemTemplate() ?? this.templateItem());
 
   private readonly _el = inject(ElementRef<HTMLElement>);
   private readonly _scrollElement: HTMLElement = this._el.nativeElement;
   private readonly _elementSize = elementSizeSignal(this._scrollElement);
+
   private readonly _visibleItemCount = computed(() =>
     this.virtual()
-      ? Math.ceil(this._elementSize().height / this.itemHeight() + this._appliedPadding() * 2)
+      ? Math.ceil(this._elementSize().height / this.itemHeight() + this.padding() * 2)
       : 0
   );
   private readonly _scrollTop = toSignal(
@@ -52,7 +85,7 @@ export class Scroller<T> {
   );
   private readonly _itemStartIndex = computed(() =>
     this.virtual()
-      ? Math.max(0, Math.ceil(this._scrollTop() / this.itemHeight()) - this._appliedPadding())
+      ? Math.max(0, Math.ceil(this._scrollTop() / this.itemHeight()) - this.padding())
       : 0
   );
   private readonly _itemEndIndex = computed(() =>
@@ -75,8 +108,31 @@ export class Scroller<T> {
       });
   });
 
+  /**
+   * Returns the last sticky item that is not rendered regularly in the list.
+   * Required because sticky items might need to be rendered longer than they would.
+   * Only applicable when virtual scrolling is enabled.
+   */
+  protected readonly latestStickyItem = computed(() => {
+    if (!this.virtual()) {
+      return null;
+    }
+    const stickyField = this.fieldSticky();
+    if (!stickyField) {
+      return null;
+    }
+    const unrenderedItemsAtTop = this.items()
+      .slice(0, this._itemStartIndex())
+      .map((item, index) => ({
+        item,
+        index,
+      }));
+    const stickyItems = unrenderedItemsAtTop.filter(item => item.item[stickyField]);
+    return stickyItems[stickyItems.length - 1] ?? null;
+  });
+
   protected readonly itemsTop = computed(() => {
-    return this._itemStartIndex() * this.itemHeight();
+    return (this._itemStartIndex() - (this.latestStickyItem() ? 1 : 0)) * this.itemHeight();
   });
 
   protected readonly dummyHeight = computed(() => {
@@ -91,6 +147,10 @@ export class Scroller<T> {
     });
   }
 
+  /**
+   * Scrolls the item with the given index into view.
+   * @param index The index of the item to scroll to.
+   */
   public scrollToIndex(index: number) {
     untracked(() => {
       if (this.virtual()) {
@@ -119,6 +179,10 @@ export class Scroller<T> {
     });
   }
 
+  /**
+   * Template types for the scroller.
+   * Can be used with the {@link NgnTemplate} directive for type safe ng-templates.
+   */
   public readonly templateTypes = templateTypesFn<{
     item: {
       $implicit: T;
