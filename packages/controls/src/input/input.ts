50eb165c52a2cd0d86fe708de8baf7ab41b69658
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2025-07-13 15:38:18 +0200

    invalid

--- packages/controls/src/input/input.ts
+++ packages/controls/src/input/input.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 import { injectThemeTemplate } from '@ngneers/controls/api';
 import { BaseDirective } from '@ngneers/controls/base';
 import { inputControlTemplate } from '@ngneers/controls-themes/templates/input';
@@ -6,9 +6,10 @@ import { inputControlTemplate } from '@ngneers/controls-themes/templates/input';
 @Directive({
   selector: 'input[ngnInput]',
   host: {
-    '[class]': 'theme.class()',
+    '[class]': 'theme.class() + (invalid() ? theme.class("invalid") : "")',
   },
 })
 export class NgnInput extends BaseDirective {
   protected readonly theme = injectThemeTemplate(inputControlTemplate);
+  public readonly invalid = input<boolean>(false);
 }
