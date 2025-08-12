b6150a7e8bfd6e310cb187a2e1d697d1e6d3bc52
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2025-08-12 19:13:00 +0200

    cleanup

--- packages/controls/src/avatar/avatar.ts
+++ packages/controls/src/avatar/avatar.ts
@@ -1,6 +1,6 @@
-import { NgClass, NgTemplateOutlet } from '@angular/common';
+import { NgClass } from '@angular/common';
 import { afterRenderEffect, Component, computed, input, signal } from '@angular/core';
-import { injectThemeTemplate, NgnTemplate } from '@ngneers/controls/api';
+import { injectThemeTemplate } from '@ngneers/controls/api';
 import { NgnBase } from '@ngneers/controls/base';
 import { NgnError } from '@ngneers/controls/utils';
 import { avatarControlTemplate } from '@ngneers/controls-themes/templates/avatar';
@@ -8,7 +8,7 @@ import { avatarControlTemplate } from '@ngneers/controls-themes/templates/avatar
 @Component({
   selector: 'ngn-avatar',
   templateUrl: './avatar.html',
-  imports: [NgTemplateOutlet, NgnTemplate, NgClass],
+  imports: [NgClass],
 })
 export class NgnAvatar extends NgnBase {
   protected readonly theme = injectThemeTemplate(avatarControlTemplate);
