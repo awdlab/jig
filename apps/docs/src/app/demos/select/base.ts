6eaa6494cf0c3a6202cef039f4102236c3c63785
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2025-08-18 22:06:53 +0200

    .

--- apps/docs/src/app/demos/select/base.ts
+++ apps/docs/src/app/demos/select/base.ts
@@ -9,8 +9,6 @@ import { exampleData } from '../../helper/data';
   template: `<ngn-select
     #select
     [options]="options"
-    [virtual]="true"
-    [itemHeight]="40"
     [popoverOptions]="{ sizeConstraints: { height: '200px' } }"
   />`,
 })
