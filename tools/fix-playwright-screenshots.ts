d03bfe2ff6d3ee7ebbc37a60c233ade8a394a848
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2025-10-14 20:58:25 +0200

    fix

--- tools/fix-playwright-screenshots.ts
+++ tools/fix-playwright-screenshots.ts
@@ -4,10 +4,12 @@
  * on windows while the rendering happens in the container, yet the screenshots are saved with the `-win32` suffix.
  */
 
-import { copyFile, readdir, stat } from 'fs/promises';
+import { readdir, rename, stat } from 'fs/promises';
 import { join } from 'path';
 
 const SNAPSHOT_DIR = 'tests/snapshots';
+
+const reverse = process.argv.includes('--reverse');
 
 async function getAllPngFilesRecursive(dir: string): Promise<string[]> {
   const entries = await readdir(dir);
@@ -30,8 +32,8 @@ async function getAllPngFilesRecursive(dir: string): Promise<string[]> {
 
 async function fixScreenshot(filePath: string) {
   // copy *-win32.png to *-linux.png
-  const newFilePath = filePath.replace('-win32.png', '-linux.png');
-  await copyFile(filePath, newFilePath);
+  const newFilePath = reverse ? filePath.replace('-linux.png', '-win32.png') : filePath.replace('-win32.png', '-linux.png');
+  await rename(filePath, newFilePath);
 }
 
 async function run() {
