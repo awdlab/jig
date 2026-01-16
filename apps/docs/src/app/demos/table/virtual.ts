fd5de9b62bfa706b1b493ab97f82fcb2c5a89fb9
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2026-01-16 18:00:35 +0100

    package updates

--- apps/docs/src/app/demos/table/virtual.ts
+++ apps/docs/src/app/demos/table/virtual.ts
@@ -14,6 +14,7 @@ import { exampleData } from '../../helper/data';
     [rows]="rows"
     [fieldId]="'id'"
     [virtual]="true"
+    [virtualPadding]="12"
     [rowHeight]="50"
   >
     <ng-template #header>
