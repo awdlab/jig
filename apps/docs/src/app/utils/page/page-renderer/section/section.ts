26fb5d1b6a36a46003cb20a6b26371484b40355a
Janik Schumacher <37637338+LoaderB0T@users.noreply.github.com>
2026-02-01 19:11:55 +0100

    overflow fixes

--- apps/docs/src/app/utils/page/page-renderer/section/section.ts
+++ apps/docs/src/app/utils/page/page-renderer/section/section.ts
@@ -8,7 +8,7 @@ import { NgnDocsMdSection } from '../../types';
   selector: 'ngn-docs-section',
   templateUrl: 'section.html',
   imports: [Md],
-  host: { class: 'w-full h-full block overflow-y-auto py-8 pr-8' },
+  host: { class: 'w-full h-full block overflow-y-auto py-8 pr-2 md:pr-8' },
 })
 export class NgnDocsPageSection {
   public readonly section = input.required<NgnDocsMdSection>();
